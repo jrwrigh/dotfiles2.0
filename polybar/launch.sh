a8af9a0aaff79ce7fb854c650e04a76e0839f4bc
James Wright <jrwrigh.iii@gmail.com>
2019-08-28 13:45:35 -0600

    Add second polybar for second display

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,7 +7,7 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
+polybar secndbar-i3 &
 polybar mainbar-i3 &
-# polybar bar2 &
 
 echo "Bars launched..."
