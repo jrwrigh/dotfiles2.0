4761dc055b69ffa0804eef3b551d6a22d60c915a
James Wright <jameswright@jameswright.xyz>
2019-11-08 15:23:19 -0700

    Update tmux_local_install with new version, XDG dirs

--- .config/tmux/tmux_local_install.sh
+++ .config/tmux/tmux_local_install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Script for installing tmux on systems where you don't have root access.
-# tmux will be installed in $HOME/local/bin.
+# tmux will be installed in $HOME/.local/bin.
 # It's assumed that wget and a C/C++ compiler are installed.
 
 # exit on error
 set -e
 
-TMUX_VERSION=2.5
+TMUX_VERSION=2.9
 
 # create our directories
-mkdir -p $HOME/local $HOME/tmux_tmp
+mkdir -p $HOME/.local $HOME/tmux_tmp
 cd $HOME/tmux_tmp
 
 # download source files for tmux, libevent, and ncurses
@@ -25,7 +25,7 @@ wget http://invisible-island.net/datafiles/release/ncurses.tar.gz
 ############
 tar xvzf libevent-2.1.8-stable.tar.gz
 cd libevent-2.1.8-stable
-./configure --prefix=$HOME/local --disable-shared
+./configure --prefix=$HOME/.local --disable-shared
 make
 make install
 cd ..
@@ -35,7 +35,7 @@ cd ..
 ############
 tar xvzf ncurses.tar.gz
 cd ncurses-6.1
-./configure --prefix=$HOME/local
+./configure --prefix=$HOME/.local
 make
 make install
 cd ..
@@ -45,12 +45,12 @@ cd ..
 ############
 tar xvzf tmux-${TMUX_VERSION}.tar.gz
 cd tmux-${TMUX_VERSION}
-./configure CFLAGS="-I$HOME/local/include -I$HOME/local/include/ncurses" LDFLAGS="-L$HOME/local/lib -L$HOME/local/include/ncurses -L$HOME/local/include"
-CPPFLAGS="-I$HOME/local/include -I$HOME/local/include/ncurses" LDFLAGS="-static -L$HOME/local/include -L$HOME/local/include/ncurses -L$HOME/local/lib" make
-cp tmux $HOME/local/bin
+./configure CFLAGS="-I$HOME/.local/include -I$HOME/.local/include/ncurses" LDFLAGS="-L$HOME/.local/lib -L$HOME/.local/include/ncurses -L$HOME/.local/include"
+CPPFLAGS="-I$HOME/.local/include -I$HOME/.local/include/ncurses" LDFLAGS="-static -L$HOME/.local/include -L$HOME/.local/include/ncurses -L$HOME/.local/lib" make
+cp tmux $HOME/.local/bin
 cd ..
 
 # cleanup
 rm -rf $HOME/tmux_tmp
 
-echo "$HOME/local/bin/tmux is now available. You can optionally add $HOME/local/bin to your PATH."
+echo "$HOME/.local/bin/tmux is now available. You can optionally add $HOME/.local/bin to your PATH."
