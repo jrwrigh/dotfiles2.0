f189bebbd82ae417eb22ca05c5da58252f21a012
James Wright <james@jameswright.xyz>
2020-03-22 11:17:12 -0600

    feat(polybar): Launch on all monitors
     - Add logging to /tmp/polybar_[MONITOR].log

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,7 +6,12 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar mainbar-i3 &
+# Launch bar on all connected monitors
+for m in $(polybar --list-monitors | cut -d":" -f1)
+do
+    polybarlogpath=/tmp/polybar_${m}.log
+    echo "---" | tee -a $polybarlogpath
+    MONITOR=$m polybar mainbar-i3 2>&1 | awk '{ print strftime("%c: "), $0; fflush(); }' | tee -a $polybarlogpath  &
+done
 
 echo "Bars launched..."
