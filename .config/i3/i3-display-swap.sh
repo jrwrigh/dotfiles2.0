e58dfdfa54bfd6eb8d17f5acb2e5cac576ee03ac
James Wright <james@jameswright.xyz>
2020-10-04 12:09:54 -0600

    style(i3): Change tabs to spaces in i3-display-swap.sh

--- .config/i3/i3-display-swap.sh
+++ .config/i3/i3-display-swap.sh
@@ -15,14 +15,14 @@ echo 'DISPLAY_CONFIG: ' $DISPLAY_CONFIG
 
 for ROW in "${DISPLAY_CONFIG[@]}"
 do
-	IFS=':'
-	read -ra CONFIG <<< "${ROW}"
-	if [ "${CONFIG[0]}" != "null" ] && [ "${CONFIG[1]}" != "null" ]; then
-		echo "moving ${CONFIG[1]} right..."
-		i3-msg -- workspace --no-auto-back-and-forth "${CONFIG[1]}"
+    IFS=':'
+    read -ra CONFIG <<< "${ROW}"
+    if [ "${CONFIG[0]}" != "null" ] && [ "${CONFIG[1]}" != "null" ]; then
+        echo "moving ${CONFIG[1]} right..."
+        i3-msg -- workspace --no-auto-back-and-forth "${CONFIG[1]}"
         sleep 0.05
-		i3-msg -- move workspace to output right
-	fi
+        i3-msg -- move workspace to output right
+    fi
 done
 
 # Focus on original workspace
