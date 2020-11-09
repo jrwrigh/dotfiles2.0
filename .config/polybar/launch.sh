2eaa5293ff5b2c53d6ed3b5837e1ba60981ac1ea
James Wright <james@jameswright.xyz>
2020-11-09 15:08:56 -0700

    feat(polybar): Put system tray on primary monitor

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,12 +6,28 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+# Find primary monitor
+PRIM_MONITOR=$(polybar --list-monitors | grep 'primary' | cut -d":" -f1)
+
 # Launch bar on all connected monitors
 for m in $(polybar --list-monitors | cut -d":" -f1)
 do
     polybarlogpath=/tmp/polybar_${m}.log
     echo "---" | tee -a $polybarlogpath
-    MONITOR=$m polybar mainbar-i3 2>&1 | \
+
+    if [ $PRIM_MONITOR = $m ]; then
+        COMMAND='polybar mainbar-i3'
+    else
+        COMMAND='polybar sidebar-i3'
+    fi
+
+    echo "PRIM_MONITOR: $PRIM_MONITOR     m: $m" |
+        awk '{ print strftime("%c: "), $0; fflush(); }' | tee -a $polybarlogpath
+
+    echo "COMMAND used: $COMMAND" |
+        awk '{ print strftime("%c: "), $0; fflush(); }' | tee -a $polybarlogpath
+
+    MONITOR=$m $COMMAND 2>&1 | \
         awk '{ print strftime("%c: "), $0; fflush(); }' | tee -a $polybarlogpath &
 done
 
