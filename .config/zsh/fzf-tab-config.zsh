b18189922a50b653bd4d21b84bd2dc2858f4b9f4
James Wright <james@jameswright.xyz>
2021-02-18 10:09:29 -0700

    feat(fzf): Add recommended config for fzf-tab-completion

--- .config/zsh/fzf-tab-config.zsh
+++ .config/zsh/fzf-tab-config.zsh
@@ -2,3 +2,8 @@
 
 zstyle ':fzf-tab:*' fzf-bindings 'bspace:backward-delete-char/eof'
 zstyle ':fzf-tab:*' continuous-trigger '/'
+
+zstyle ':completion:*:git-checkout:*' sort false
+zstyle ':completion:*:descriptions' format '[%d]'
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+zstyle ':fzf-tab:*' switch-group ',' '.'
