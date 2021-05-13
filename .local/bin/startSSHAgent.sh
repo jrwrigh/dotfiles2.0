e7e53614b93a94d630c10d37e73c78274255a042
James Wright <james@jameswright.xyz>
2021-05-13 10:36:03 -0600

    feat(ssh-agent): Implement ssh-agent socket solution
     - Use sockets instead of environment variables to allow ssh-agent to
       work flexibly across tmux sessions

--- .local/bin/startSSHAgent.sh
+++ .local/bin/startSSHAgent.sh
@@ -1,26 +1,5 @@
 ## Run via `source .local/bin/startSSHAgent.sh` inside .zprofile
 
-# Start the ssh-agent
-# From https://stackoverflow.com/a/18915067/7564988 and http://mah.everybody.org/docs/ssh#run-ssh-agent
-SSH_ENV="$HOME/.ssh/environment"
+[ -n "${SSH_AUTH_SOCK+1}" ] || export SSH_AUTH_SOCK=$XDG_RUNTIME_DIR/ssh-agent.socket
 
-function start_agent {
-     echo "Initialising new SSH agent..."
-     /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
-     echo succeeded
-     chmod 600 "${SSH_ENV}"
-     . "${SSH_ENV}" > /dev/null
-     /usr/bin/ssh-add;
-}
-
-# Source SSH settings, if applicable
-
-if [ -f "${SSH_ENV}" ]; then
-     . "${SSH_ENV}" > /dev/null
-     #ps ${SSH_AGENT_PID} doesn't work under cywgin
-     ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
-         start_agent;
-     }
-else
-     start_agent;
-fi
+eval `ssh-agent -s -a $SSH_AUTH_SOCK`
