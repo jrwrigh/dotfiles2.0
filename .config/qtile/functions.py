1cc0e2588959a9275379f5099f3de45bd7684cfa
James Wright <james@jameswright.xyz>
2021-05-25 20:45:50 -0600

    feat(qtile): Initial commit of qtile config

---
+++ .config/qtile/functions.py
@@ -0,0 +1,95 @@
+from libqtile.lazy import lazy
+from libqtile.log_utils import logger
+
+    # Display handling
+from Xlib import display as xdisplay
+
+def get_num_monitors():
+    """Get the number of monitors activated on the computer"""
+    num_monitors = 0
+    try:
+        display = xdisplay.Display()
+        screen = display.screen()
+        resources = screen.root.xrandr_get_screen_resources()._data
+
+        for output in resources['outputs']:
+            monitor = display.xrandr_get_output_info(output, resources['config_timestamp'])._data
+            preferred = False
+            if "preferred" in monitor.keys():
+                preferred = monitor['preferred']
+            elif "num_preferred" in monitor.keys():
+                preferred = monitor['num_preferred']
+            if preferred:
+                num_monitors += 1
+    except Exception:
+        # always setup at least one monitor
+        return 1
+    else:
+        return num_monitors
+
+
+def ungrab_chord():
+    """Shortened version for ungrabing keychords"""
+    @lazy.function
+    def _ungrab_chord(qtile):
+        qtile.ungrab_chord()
+    return _ungrab_chord
+
+
+def warp_cursor_here():
+    '''Warp the cursor to the currently focused window'''
+    @lazy.function
+    def _warp_cursor_here(qtile):
+        qtile.current_window.focus(warp=True)
+    return _warp_cursor_here
+
+
+def swap_next_screen():
+    """Doesn't work"""
+    @lazy.function
+    def _swap_next_screen(qtile):
+        if len(qtile.screens) != 2: return
+        i = qtile.screens.index(qtile.current_screen)
+
+        if qtile.current_group:
+            group = qtile.screens[i - 1].group
+            qtile.groups[group.name].cmd_toscreen()
+    return _swap_next_screen
+
+
+def move_next_screen():
+    @lazy.function
+    def _move_next_screen(qtile):
+        if len(qtile.screens) != 2: return
+        i = qtile.screens.index(qtile.current_screen)
+        j = 0 if i == 1 else 1
+
+        if qtile.current_group:
+            group = qtile.current_group
+            logger.warning(f'Move group "{group.name}" from screen {i}->{j}')
+            qtile.cmd_to_screen(j)
+            group.cmd_toscreen()
+
+    return _move_next_screen
+
+
+def viewGroup(name):
+    '''Smarter group view switching
+
+    Display group on current screen if not otherwise displayed. If group is
+    already displayed, switch focus to that screen
+
+    Modified from: https://gist.github.com/TauPan/9c09bd9defc5ac3c9e06#file-config-py-L86-L103
+    '''
+    @lazy.function
+    def _viewGroup(qtile):
+
+        group = qtile.groups_map[name]
+        if group != qtile.current_group:
+            if group.screen:
+                qtile.cmd_to_screen(group.screen.index)
+
+            else:
+                group.cmd_toscreen()
+
+    return _viewGroup
---
+++ .config/qtile/theme.py
@@ -0,0 +1,34 @@
+
+class Theme(object):
+    white       = "#e8e8e3"
+    white2      = "#d8d8d3"
+    black       = "#272822"
+    lightblack  = "#2d2e27"
+    lightblack2 = "#383a3e"
+    lightblack3 = "#3f4145"
+    darkblack   = "#211f1c"
+    grey        = "#8f908a"
+    lightgrey   = "#575b61"
+    darkgrey    = "#64645e"
+    warmgrey    = "#75715e"
+
+    pink        = "#dc2566"
+    green       = "#a6e22d"
+    aqua        = "#66d9ef"
+    yellow      = "#e6db74"
+    orange      = '#fd9720'
+    purple      = "#ae81ff"
+    red         = "#e73c50"
+    purered     = "#ff0000"
+    darkred     = "#5f0000"
+
+    br_green    = "#9ec400"
+    br_yellow   = "#e7c547"
+    br_blue     = "#7aa6da"
+    br_purple   = "#b77ee0"
+    br_cyan     = "#54ced6"
+    br_white    = "#ffffff"
+
+monokai = Theme()
+
+
