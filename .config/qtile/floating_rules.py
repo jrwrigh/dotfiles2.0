5dba9e4cdeaae78db43fd0e63ae3c4383e57fe04
James Wright <james@jameswright.xyz>
2021-06-22 15:23:20 -0600

    feat(qtile): Add NetworkManager to floating

--- .config/qtile/floating_rules.py
+++ .config/qtile/floating_rules.py
@@ -52,4 +52,5 @@ floating_matches = [
     Match(wm_class='Gnucash', title=re.compile(r'(?!.* - GnuCash)')),
     Match(title='LastPass: Free Password Manager'),
     Match(wm_class='kcharselect'),
+    Match(wm_class='Nm-connection-editor', title='Network Connections'),
 ]
