74f5b9d79c9cf33caa1fe307c2f0f25e91a2d52b
James Wright <james@jameswright.xyz>
2022-06-25 15:56:42 -0600

    nvim: Update init.lua to be more modular

---
+++ .config/nvim/lua/options.lua
@@ -0,0 +1,114 @@
+
+vim.opt.number         = true
+vim.opt.relativenumber = true
+vim.opt.expandtab      = true -- Insert spaces when <TAB> is pressed
+vim.opt.tabstop        = 4    -- Set number of spaces inserted when <TAB> is pressed
+vim.opt.shiftwidth     = 4    -- Indentation amount when using << and >>
+vim.opt.shiftround     = true -- Makes the shift command move in multiples of shiftwidth
+vim.opt.splitbelow     = true -- Horizontal split below current
+vim.opt.splitright     = true -- Vetical split to right of current
+vim.opt.cursorline     = true
+vim.opt.scrolloff      = 2
+
+vim.opt.showbreak = '↳    '
+vim.opt.listchars:append { tab= '⇄ ', trail= '␣', extends= '❯', precedes= '❮' }
+vim.go.termguicolors = true   -- Allow use of non-256 colorschemes
+
+vim.opt.inccommand = 'nosplit' -- Incremental live completion
+vim.opt.hlsearch   = true      -- Set highlight on search
+vim.opt.hidden     = true      -- Do not save when switching buffers
+vim.o.breakindent  = true      -- Enable break indent
+vim.o.undofile     = true      -- Save undo history
+vim.o.ignorecase   = true      -- Case insensitive searching ...
+vim.o.smartcase    = true      -- ... UNLESS /C or capital in search
+
+--Decrease update time
+vim.o.updatetime  = 250
+vim.wo.signcolumn = 'yes'
+
+local api = vim.api
+-- Highlight on yank
+local yankGrp = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+api.nvim_create_autocmd("TextYankPost", {
+  group = yankGrp,
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  desc = "Highlight yank",
+})
+
+--#######################################################
+----------------- FILE TYPE OVERRIDES -------------------
+--#######################################################
+
+-- vim.go.load_doxygen_syntax = true
+vim.cmd [[let g:load_doxygen_syntax = 1]]
+vim.cmd [[
+autocmd FileType c call SetCOptions()
+autocmd FileType cpp call SetCppOptions()
+autocmd FileType fortran call SetFortranOptions()
+autocmd FileType xdefaults call SetXdefaultsOptions()
+autocmd FileType text,markdown setlocal spell
+autocmd FileType markdown call SetMarkdownOptions()
+autocmd FileType git call SetGitOptions()
+autocmd FileType lua call SetLuaOptions()
+
+source ~/.config/nvim/i3config_folding.vim
+source ~/.config/nvim/vim_folding.vim
+
+function! SetCOptions()
+    setlocal expandtab
+    setlocal tabstop=2
+    setlocal shiftwidth=2
+    setlocal commentstring=//\ %s
+endfunction
+
+function! SetCppOptions()
+    setlocal expandtab
+    setlocal tabstop=2
+    setlocal shiftwidth=2
+    setlocal commentstring=//\ %s
+endfunction
+
+function! SetFortranOptions()
+    setlocal expandtab
+    setlocal tabstop=2
+    setlocal shiftwidth=2
+    :syn clear fortranSerialNumber
+endfunction
+
+function! SetLuaOptions()
+    setlocal expandtab
+    setlocal tabstop=2
+    setlocal shiftwidth=2
+endfunction
+
+function! SetXdefaultsOptions()
+    setlocal commentstring=!\ %s
+endfunction
+
+function! SetMarkdownOptions()
+    let b:indentLine_enabled = 0
+endfunction
+
+function! SetGitOptions()
+    setlocal foldlevel=20
+endfunction
+
+" Set file type for phasta input files
+augroup inp_ft
+  au!
+  autocmd BufNewFile,BufRead *.inp   set ft=conf
+augroup END ]]
+
+-- vim.api.nvim_create_autocmd({'FileType'}, {
+--     pattern = 'c',
+--     command = 'SetCOptions()'
+-- })
+
+vim.api.nvim_create_autocmd({'BufRead','BufNewFile'}, {
+    pattern = {"*.h"},
+    callback = function() vim.o.filetype='c' end
+})
+
---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,102 @@
+local packer_group = vim.api.nvim_create_augroup("Packer", { clear = true })
+vim.api.nvim_create_autocmd(
+  "BufWritePost",
+  { command = "source <afile> | PackerCompile", group = packer_group, pattern = "plugins.lua" }
+)
+
+local fn = vim.fn
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+  Packer_bootstrap = fn.system({
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  })
+end
+vim.api.nvim_command("packadd packer.nvim")
+-- returns the require for use in `config` parameter of packer's use
+-- expects the name of the config file
+local function get_setup(name)
+  return string.format('require("setup/%s")', name)
+end
+
+return require("packer").startup({
+  function(use)
+    use 'wbthomason/packer.nvim' -- Package manager
+    use { 'numToStr/Comment.nvim', config = get_setup('comment') }
+    use { 'nvim-telescope/telescope.nvim',
+      -- cmd      = 'Telescope',
+      -- module   = {'telescope', 'telescope.builtin'},
+      requires = {
+        { 'nvim-lua/popup.nvim' },
+        { 'nvim-lua/plenary.nvim' },
+        { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+      },
+      config = get_setup('telescope')
+    }
+    use { 'tanvirtin/monokai.nvim', config = get_setup('monokai') }
+    use { 'lukas-reineke/indent-blankline.nvim', config = get_setup('indent_blankline')}
+    use { 'lewis6991/gitsigns.nvim',
+      requires = { 'nvim-lua/plenary.nvim' },
+      config = get_setup('gitsigns'),
+    }
+
+    use { 'nvim-treesitter/nvim-treesitter',
+      config = get_setup('treesitter'),
+      run    = ':TsUpdate',
+    }
+    use { 'nvim-treesitter/nvim-treesitter-textobjects' }
+
+    use { 'hrsh7th/nvim-cmp',
+      requires = {
+        { 'hrsh7th/cmp-nvim-lsp' },
+        { 'hrsh7th/cmp-buffer' },
+        { 'hrsh7th/cmp-path' },
+        { 'hrsh7th/cmp-cmdline' },
+        { 'hrsh7th/cmp-nvim-lsp-signature-help' },
+        { 'saadparwaiz1/cmp_luasnip' },
+      },
+      config = get_setup('cmp'),
+    }
+
+    use 'L3MON4D3/LuaSnip' -- Snippets plugin
+    use { 'neovim/nvim-lspconfig',
+      requires = {
+        { "williamboman/nvim-lsp-installer", config = get_setup('lsp-installer') }
+      },
+      config = get_setup('lsp'),
+    }
+    use {
+      'nvim-lualine/lualine.nvim',
+      requires = { 'kyazdani42/nvim-web-devicons', opt = true },
+      config = get_setup('lualine'),
+      after = 'monokai.nvim',
+    }
+    use { 'junegunn/vim-easy-align', config = get_setup('easy_align')}
+    use 'chrisbra/improvedft'
+    use 'tpope/vim-surround'
+    use 'tpope/vim-repeat'
+    use 'tpope/vim-git' -- plugin/syntax stuff for git files (commit, rebase -i, etc)
+    use { 'tpope/vim-fugitive', tag='*' } -- Git commands in nvim
+    use { 'rbong/vim-flog', branch='master' }  -- for browsing git log branches
+    -- use 'vim-airline/vim-airline'
+    -- use 'vim-airline/vim-airline-themes'
+    use 'svermeulen/vim-subversive' --adds substitute commmmands to paste over a text object (ie. `siw`)
+
+    if Packer_bootstrap then
+      require("packer").sync()
+    end
+  end,
+  config = {
+    display = {
+      open_fn = require("packer.util").float,
+    },
+    profile = {
+      enable = true,
+      threshold = 1, -- the amount in ms that a plugins load time must be over for it to be included in the profile
+    },
+  },
+})
