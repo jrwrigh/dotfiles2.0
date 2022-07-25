6f8df364ced758de844cf91b91e8c128194228f7
James Wright <james@jameswright.xyz>
2022-07-25 08:52:42 -0600

    nvim: Move to mason.nvim from lsp-installer

---
+++ .config/nvim/lua/setup/mason.lua
@@ -0,0 +1,11 @@
+require("mason").setup {
+    ui = {
+        icons = {
+            package_installed = "✓"
+        }
+    }
+}
+
+require("mason-lspconfig").setup {
+    ensure_installed = { "sumneko_lua" },
+}
