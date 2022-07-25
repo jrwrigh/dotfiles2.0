77cb9baf8b472072d5f350e111c5bd1291d64c2b
James Wright <james@jameswright.xyz>
2022-07-25 08:56:54 -0600

    nvim: Setup luasnip

---
+++ .config/nvim/lua/setup/luasnip.lua
@@ -0,0 +1,32 @@
+local ls = require('luasnip')
+
+ls.config.set_config {
+
+  history = true,
+  updateevents = "TextChanged,TextChangedI"
+
+}
+
+-- <c-k> is my expansion key
+-- this will expand the current item or jump to the next item within the snippet.
+vim.keymap.set({ "i", "s" }, "<c-k>", function()
+  if ls.expand_or_jumpable() then
+    ls.expand_or_jump()
+  end
+end, { silent = true })
+
+-- <c-j> is my jump backwards key.
+-- this always moves to the previous item within the snippet
+vim.keymap.set({ "i", "s" }, "<c-j>", function()
+  if ls.jumpable(-1) then
+    ls.jump(-1)
+  end
+end, { silent = true })
+
+-- <c-l> is selecting within a list of options.
+-- This is useful for choice nodes (introduced in the forthcoming episode 2)
+vim.keymap.set("i", "<c-l>", function()
+  if ls.choice_active() then
+    ls.change_choice(1)
+  end
+end)
