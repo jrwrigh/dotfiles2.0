513d3eb92cfeb0b4f8ebacf3bf1e53594ed3a94f
James Wright <james@jameswright.xyz>
2022-08-15 15:24:55 -0600

    qtile: Comment out start-pulseaudio-x11 from autostart

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -3,7 +3,7 @@
 date >> $HOME/temp/qtileautostart_test
 
 udiskie -A --notify --smart-tray &
-start-pulseaudio-x11 &
+# start-pulseaudio-x11 &
 pa-applet &!
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 nitrogen --restore; sleep 1; picom -b &
