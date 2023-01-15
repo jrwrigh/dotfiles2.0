bf389ee8a54e2f38e9a56e29a2da61066fb5528d
James Wright <james@jameswright.xyz>
2023-01-15 16:16:45 -0700

    feat(nvim): Disable neoscroll for zt, zz, zb

--- .config/nvim/lua/setup/neoscroll.lua
+++ .config/nvim/lua/setup/neoscroll.lua
@@ -1,5 +1,6 @@
 require('neoscroll').setup({
     -- All these keys will be mapped to their corresponding default scrolling animation
+    mappings = {},
     hide_cursor = true,          -- Hide cursor while scrolling
     stop_eof = true,             -- Stop at <EOF> when scrolling downwards
     respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
@@ -16,8 +17,5 @@ t['<C-u>'] = {'scroll', {'-vim.wo.scroll', 'true', '150'}}
 t['<C-d>'] = {'scroll', { 'vim.wo.scroll', 'true', '150'}}
 t['<C-b>'] = {'scroll', {'-vim.api.nvim_win_get_height(0)', 'true', '150'}}
 t['<C-f>'] = {'scroll', { 'vim.api.nvim_win_get_height(0)', 'true', '150'}}
-t['zt']    = {'zt', {'150'}}
-t['zz']    = {'zz', {'150'}}
-t['zb']    = {'zb', {'150'}}
 
 require('neoscroll.config').set_mappings(t)
