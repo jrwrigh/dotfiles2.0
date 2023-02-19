5dab413a9b50ff54e780d425a53462ce09fabf9b
James Wright <james@jameswright.xyz>
2023-02-19 16:17:12 -0700

    feat(nvim): Add completion source to cmp menu

--- .config/nvim/lua/setup/cmp.lua
+++ .config/nvim/lua/setup/cmp.lua
@@ -61,10 +61,26 @@ cmp.setup({
   formatting = {
     fields = { "kind", "abbr", "menu" },
     format = function(entry, vim_item)
-      local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50, ellipsis_char = '...' })(entry, vim_item)
+      local kind = require("lspkind").cmp_format({
+        mode = "symbol_text",
+        maxwidth = 50,
+        ellipsis_char = '...',
+        menu = ({
+          buffer        = "[BUF]",
+          nvim_lsp      = "[LSP]",
+          luasnip       = "[SNP]",
+          nvim_lua      = "[LUA]",
+          latex_symbols = "[TEX]",
+          path          = "[PTH]",
+        })
+      })(entry, vim_item)
       local strings = vim.split(kind.kind, "%s", { trimempty = true })
       kind.kind = " " .. (strings[1] or "") .. " "
-      kind.menu = "    (" .. (strings[2] or "") .. ")"
+      if kind.menu then
+        kind.menu = "    (" .. (strings[2] or "") .. ") " .. kind.menu
+      else
+        kind.menu = "    (" .. (strings[2] or "") .. ")"
+      end
 
       return kind
     end,
@@ -85,6 +101,7 @@ cmp.setup({
     { name = 'luasnip' }, -- For luasnip users.
     { name = 'nvim_lsp_signature_help' },
   }, {
+    { name = 'path' },
     { name = 'buffer' },
   }),
   sorting = {
