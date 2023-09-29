1ae6800505f99c0842bc3f2763d6772460f484a1
James Wright <james@jameswright.xyz>
2023-09-29 13:32:06 -0600

    nvim: Update indent_blankline for version 3
    See https://github.com/lukas-reineke/indent-blankline.nvim/wiki/Migrate-to-version-3

--- .config/nvim/lua/setup/indent_blankline.lua
+++ .config/nvim/lua/setup/indent_blankline.lua
@@ -1,8 +1,11 @@
-require("indent_blankline").setup {
-  -- for example, context is off by default, use this to turn it on
-  char = "┊",
-  char_list = {'¦', '┊', '┊', '┊', '┊', '┊', '┊', '┊', '┊', '┊'},
-  context_char="│",
-  show_current_context = true,
-  show_current_context_start = true,
+require("ibl").setup {
+  indent = {
+    char = {'¦', '┊', '┊', '┊', '┊', '┊', '┊', '┊', '┊', '┊'},
+  },
+  scope = {
+    enabled = true,
+    char = "│",
+    show_start = true,
+    show_end = false,
+  }
 }
