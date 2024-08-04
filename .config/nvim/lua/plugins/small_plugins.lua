6af4a8158f5cf53ba8a9612d0d3da36c988a3b82
James Wright <james@jameswright.xyz>
2024-08-04 09:17:46 -0600

    feat(nvim): Change Doxygen NeoGen settings
    Makes the auto-docstring plugin format more similar to what I want.

--- .config/nvim/lua/plugins/small_plugins.lua
+++ .config/nvim/lua/plugins/small_plugins.lua
@@ -14,7 +14,32 @@ return {
   { "danymat/neogen",
     event = "VeryLazy",
     dependencies = "nvim-treesitter/nvim-treesitter",
-    opts = {},
+    opts = {
+      languages = {
+        c = {
+          template = {
+            annotation_convention = "my_doxygen",
+            my_doxygen = {
+              { nil, "/**",          { no_results = true, type = { "func", "file", "class" } } },
+              { nil, "  @file",     { no_results = true, type = { "file" } } },
+              { nil, "  @brief $1", { no_results = true, type = { "func", "file", "class" } } },
+              { nil, "**/",          { no_results = true, type = { "func", "file", "class" } } },
+              { nil, "",             { no_results = true, type = { "file" } } },
+
+              { nil,                "/**",            { type = { "func", "class", "type" } } },
+              { "class_name",       "  @class %s",   { type = { "class" } } },
+              { "type",             "  @typedef %s", { type = { "type" } } },
+              { nil,                "  @brief $1",   { type = { "func", "class", "type" } } },
+              { nil,                "",               { type = { "func", "class", "type" } } },
+              { "tparam",           "  @tparam %s $1" },
+              { "parameters",       "  @param %s $1" },
+              { "return_statement", "  @return $1" },
+              { nil,                "**/",            { type = { "func", "class", "type" } } },
+            },
+          },
+        },
+      },
+    },
   },
 
   { 'lukas-reineke/indent-blankline.nvim',
