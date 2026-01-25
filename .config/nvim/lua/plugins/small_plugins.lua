9b9b29f9243f798f16c8d6644a455260ff6033d3
James Wright <james@jameswright.xyz>
2026-01-25 11:01:23 -0700

    feat(nvim): Remove neoscroll
    Ends up lagging quite a bit and I've gotten used to the normal page up
    and down motions

--- .config/nvim/lua/plugins/small_plugins.lua
+++ .config/nvim/lua/plugins/small_plugins.lua
@@ -170,35 +170,6 @@ return {
     end
   },
 
-  { 'karb94/neoscroll.nvim',
-    config = function()
-      require('neoscroll').setup({
-          -- All these keys will be mapped to their corresponding default scrolling animation
-          mappings = {},
-          hide_cursor = true,          -- Hide cursor while scrolling
-          stop_eof = true,             -- Stop at <EOF> when scrolling downwards
-          respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
-          cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
-          easing_function = 'quartic',       -- Default easing function
-          pre_hook = nil,              -- Function to run before the scrolling animation starts
-          post_hook = nil,             -- Function to run after the scrolling animation ends
-          performance_mode = false,    -- Disable "Performance Mode" on all buffers.
-      })
-
-      neoscroll = require('neoscroll')
-      local keymap = {
-        ["<C-u>"] = function() neoscroll.ctrl_u({ duration = 150 }) end;
-        ["<C-d>"] = function() neoscroll.ctrl_d({ duration = 150 }) end;
-        ["<C-b>"] = function() neoscroll.ctrl_b({ duration = 150 }) end;
-        ["<C-f>"] = function() neoscroll.ctrl_f({ duration = 150 }) end;
-      }
-      local modes = { 'n', 'v', 'x' }
-      for key, func in pairs(keymap) do
-        vim.keymap.set(modes, key, func)
-      end
-    end,
-  },
-
   { 'lervag/vimtex',
     config = function()
       vim.g.vimtex_view_method = 'zathura'
